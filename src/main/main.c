03dcea4415fac1b3fe43d22a65c70f6f78a61c41
NotExactlySiev <sadrabousi@gmail.com>
2023-05-19 00:42:11 +0330

    more cleanup. things are at their right places now

--- src/main/main.c
+++ src/main/main.c
@@ -1,67 +1,122 @@
 #include "common.h"
 #include "main.h"
 
+// file execute loop
 INCLUDE_ASM("asm/nonmatchings/main", func_800188C8);
 
-INCLUDE_ASM("asm/nonmatchings/main", get_file_addr);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_80018A6C);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_80018A7C);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_80018A8C);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_80018AB4);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_8001926C);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_80019680);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_8001972C);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_800197C8);
-
-INCLUDE_ASM("asm/nonmatchings/main", func_8001983C);
-
-void nop(void) {
+char* get_file_addr(s32 idx) {
+    if (idx > 42) return 0;
+    return g_Files[idx].addr;
 }
 
-INCLUDE_ASM("asm/nonmatchings/main", flush_cache_safe);
+s32 func_80018A6C(void) {
+    return D_80047D50;
+}
 
-INCLUDE_ASM("asm/nonmatchings/main", jt_clear);
+s32 func_80018A7C(void) {
+    return D_80047D4C;
+}
 
-INCLUDE_ASM("asm/nonmatchings/main", jt_set);
+void func_80018A8C(s32 arg0) {
+    if (arg0 != 0)
+        D_80047D4C = 1;
+    else
+        D_80047D4C = 0;
+}
 
-INCLUDE_ASM("asm/nonmatchings/main", func_80019948);
+// 2 very massive functions
+INCLUDE_ASM("asm/nonmatchings/10C8", func_80018AB4);
 
-INCLUDE_ASM("asm/nonmatchings/main", func_80019990);
+INCLUDE_ASM("asm/nonmatchings/10C8", func_8001926C);
 
-INCLUDE_ASM("asm/nonmatchings/main", func_800199D4);
+// some setup thing
+INCLUDE_ASM("asm/nonmatchings/10C8", func_80019680);
 
-INCLUDE_ASM("asm/nonmatchings/main", func_800199E4);
+// 3 timer event functions
+INCLUDE_ASM("asm/nonmatchings/10C8", func_8001972C);
 
-INCLUDE_ASM("asm/nonmatchings/main", func_80019AE8);
+INCLUDE_ASM("asm/nonmatchings/10C8", func_800197C8);
 
-INCLUDE_ASM("asm/nonmatchings/main", func_80019AF8);
+INCLUDE_ASM("asm/nonmatchings/10C8", func_8001983C);
 
-INCLUDE_ASM("asm/nonmatchings/main", func_80019B1C);
+void nop(void) {}
 
-INCLUDE_ASM("asm/nonmatchings/main", get_D_80047E6C);
+void flush_cache_safe(void) {
+    k_EnterCriticalSection();
+    k_FlushCache();
+    k_ExitCriticalSection();
+}
 
-INCLUDE_ASM("asm/nonmatchings/main", jt_reset);
 
-INCLUDE_ASM("asm/nonmatchings/main", func_80019D0C);
+// NONMATCHING
+void jt_clear(void) {
+   
+    int i;
+    for (i = 0; i < 1024; i++)
+    {
+        jmptable[i] = KSEG0((u32) nop);
+    }
+    flush_cache_safe();
+}
 
+// NONMATCHING
+void jt_set(void* func, s32 idx) {
+    jmptable[idx] = KSEG0(func);
+    flush_cache_safe();
+}
+
+// 2 timer functions
+INCLUDE_ASM("asm/nonmatchings/10C8", func_80019948);
+
+INCLUDE_ASM("asm/nonmatchings/10C8", func_80019990);
+
+// 4 functions related to game version
+INCLUDE_ASM("asm/nonmatchings/10C8", func_800199D4);
+
+INCLUDE_ASM("asm/nonmatchings/10C8", func_800199E4);
+
+INCLUDE_ASM("asm/nonmatchings/10C8", func_80019AE8);
+
+INCLUDE_ASM("asm/nonmatchings/10C8", func_80019AF8);
+
+// 1 setup function (sets up jumptable)
+INCLUDE_ASM("asm/nonmatchings/10C8", func_80019B1C); // jumptable and interrupts?
+
+
+s32 get_D_80047E6C(void) {
+    return D_80047E6C;
+}
+
+void* jt_reset(void) {
+    jt_clear();
+    jt_set(func_80019DCC, 0xFF);
+    jt_set(get_D_80047E6C, 0x2);
+    jt_set(get_file_addr, 0x6);
+    return func_80019B1C;
+}
+
+
+INCLUDE_ASM("asm/nonmatchings/main", func_80019D0C); // has a loop through jt
+
+// 2 exception functions
 INCLUDE_ASM("asm/nonmatchings/main", func_80019D64);
 
 INCLUDE_ASM("asm/nonmatchings/main", func_80019D78);
 
-INCLUDE_ASM("asm/nonmatchings/main", func_80019DCC);
+u32 func_80019DCC(void) {
+  return 0x10002;
+}
 
-INCLUDE_ASM("asm/nonmatchings/main", setNextFile);
+void setNextFile(s32 id) {
+    g_NextFile = id;    
+}
 
-INCLUDE_ASM("asm/nonmatchings/main", getNextFile);
+s32 getNextFile() {
+    return g_NextFile;
+}
 
-INCLUDE_ASM("asm/nonmatchings/main", getGameConfig);
+u8* getGameConfig() { //func_80019DF8
+    return &g_GameConfig;
+}
 
 INCLUDE_ASM("asm/nonmatchings/main", main);
