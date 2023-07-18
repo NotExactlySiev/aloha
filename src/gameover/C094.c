35fb8cada0ca06a21873a44b5981d01d267fcf34
NotExactlySiev <sadrabousi@gmail.com>
2023-07-19 00:02:16 +0330

    forgot some shit

---
+++ src/gameover/C094.c
@@ -0,0 +1,43 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EB894);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EB8C0);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EB8F8);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBA08);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBA20);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBA40);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBCE0);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBCF0);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBD10);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBD5C);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBEA8);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBEB8);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EBF58);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EC098);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EC128);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EC14C);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EC23C);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EC268);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EC318);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EC358);
+
+INCLUDE_ASM("asm/gameover/nonmatchings/C094", func_800EC3A4);
