b7076d92a007092576ba90f5f7c4e67262801590
NotExactlySiev <sadrabousi@gmail.com>
2023-08-10 22:11:18 +0330

    fixed makecd.sh

--- makecd.sh
+++ makecd.sh
@@ -1,1 +1,1 @@
-make build/main.exe && cd .. && cp splat/build/main.exe usa/SCUS_941.03 && mkpsxiso -y usa.xml
+make build/main.exe && cd .. && cp splat/build/main.exe usa2/SCUS_941.03 && mkpsxiso -o usaregen.bin -y usa2.xml
