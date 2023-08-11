454f7532146c8fdc9c6072b928ff0a77b383bd7c
NotExactlySiev <sadrabousi@gmail.com>
2023-08-11 17:53:06 +0330

    more than half of libspu labled

--- src/main/libsnd.c
+++ src/main/libsnd.c
@@ -1,60 +1,60 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002C4A8);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002C4A8);     // SpuSetVoiceAttr
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002C4D0);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002C4D0);     // _SpuSetVoiceAttr
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002CBCC);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002CBCC);     // SpuGetKeyStatus
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002CC60);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002CC60);     // _spu_note2pitch
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002CD54);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002CD54);     // _spu_pitch2note
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002CEAC);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002CEAC);     // SpuSetReverbModeParam
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002D3C0);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002D3C0);     // _spu_gcSPU
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002D6C0);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002D6C0);     // SpuInitMalloc
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002D730);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002D730);     // SpuMalloc
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002D9FC);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002D9FC);     // _SpuMallocSparateTo3
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002DD58);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002DD58);     // SpuMallocWithStartAddr
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E008);
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E078);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E078);     // _SpuIsInAllocateArea_
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E0F0);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E0F0);     // SpuFree
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E16C);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E16C);     // _spu_print
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E21C);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E21C);     // SpuInit
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E2E4);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E2E4);     // SpuInitHot
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E3AC);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E3AC);     // SpuStart
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E42C);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E42C);     // SpuQuit
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E49C);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E49C);     // DisableEvent (two)
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E53C);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002E53C);     // _spu_init
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EAC8);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EAC8);     // _SpuClearCallbacks
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EAFC);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EAFC);     // SpuDataCallback
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EB28);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EB28);     // _spu_write
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EB90);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EB90);     // _spu_writeByIO
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EED4);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EED4);     
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EF00);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002EF00);     // _spu_FiDMA
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002F020);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002F020);     // 
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002F288);
 
@@ -64,7 +64,7 @@ INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002F440);
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002FE24);
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002FFA8);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8002FFA8);     // set_SPU_regs
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80030478);
 
@@ -74,11 +74,11 @@ INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8003093C);
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8003095C);
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80030978);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80030978);     // _SpuDataCallback
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8003099C);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8003099C);     // set_spu_irq_handler
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800309C0);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800309C0);     // 
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800309F4);
 
@@ -88,23 +88,23 @@ INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80030A98);
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", SpuSetCommonAttr);
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80030E94);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80030E94);     // SpuClearReverbWorkArea
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031064);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031064);     // WaitEvent
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031074);
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8003109C);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8003109C);     // SpuSetReverbDepth
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_8003111C);
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031184);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031184);     // SpuSetReverb
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031254);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031254);     // SpuIsTransferCompleted
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031308);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031308);     // _spu_setInTransfer
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031334);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031334);     // _spu_getInTransfer
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031344);
 
@@ -112,18 +112,18 @@ INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800313A0);
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800314FC);
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031550);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031550);     // SpuSetIRQAddr
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800315B4);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800315B4);     // SpuWrite
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031614);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031614);     
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800316BC);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800316BC);     // SpuSetKeyOnWithAttr
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800316F0);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800316F0);     // SpuSetKey
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031770);
 
-INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031798);
+INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_80031798);     // SpuReserveReverbWorkArea
 
 INCLUDE_ASM("asm/main/nonmatchings/libsnd", func_800317E8);
