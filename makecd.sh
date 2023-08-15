12093bc2b3e7e0b1f83f9100c856e2c302964706
NotExactlySiev <sadrabousi@gmail.com>
2023-08-15 19:30:59 +0330

    clean up

--- makecd.sh
+++
@@ -1,4 +0,0 @@
-make build/gameover.exe build/main.exe && cd .. &&
-./jfcomp comp splat/build/gameover.exe usa2/GAMEOVER.PEX &&
-cp splat/build/main.exe usa2/SCUS_941.03 &&
-mkpsxiso -o usaregen.bin -y usa2.xml
